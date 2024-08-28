56702407738fd9b34e7ccab97b020d4562c205f9
Petrus <petrusheya@gmail.com>
2024-08-28 07:59:14 +0200

    Import statement

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const WebSocket = require('ws');
 const cors = require('cors');
 const { v4: uuidv4 } = require('uuid');
-const { default: Prometheus } = require('./utilis.js');
+const Prometheus = require('./utilis.js');
 
 
 const app = express();
